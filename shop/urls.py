083eb0248d11c36a601a47b8f0b69df665fb7608
TatiPav <64731979+TatiPav@users.noreply.github.com>
2020-06-07 01:51:30 +0300

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -20,9 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('cart/', include('cart.urls', namespace='cart')),
-    # path('orders/', include('orders.urls', namespace='orders')),
+    path('admin/', admin.site.urls),  
     path('', include('shops.urls', namespace='shops')),
 ]
 
